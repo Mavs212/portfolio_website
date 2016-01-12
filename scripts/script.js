2bc8d8822c599bb01a04b41ea8348235ba87f9f9
David Schwartz <dbschwartz2@gmail.com>
2016-01-12 10:28:56 -0700

    with sound

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('#continue').hover(function(){
+        $(this).toggleClass('highlight');
+  });
+
+});
+
+/*
+
+(document).ready(function() {
+  $('.continue').mouseover(function(){
+    $(this).addClass('highlight');
+  });
+  $('.continue').mouseout(function(){
+      $(this).removeClass('highlight');
+  });
+});
+*/
